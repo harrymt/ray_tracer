f5b7326a2bf86f4caa79c06b75be2f74da1507b6
Harry Mumford-Turner <hm16679@it052561.users.bris.ac.uk>
2017-01-23 12:41:34 +0000

    Colour interpolation to make rainbow

--- Source/main.cpp
+++ Source/main.cpp
@@ -16,7 +16,16 @@ const int SCREEN_WIDTH = 500;
 const int SCREEN_HEIGHT = 500;
 SDL_Surface* screen;
 int t;
-double colour = 0.0; bool colour_increase = true;
+
+vec3 topLeft(1,0,0);     // red
+vec3 topRight(0,0,1);    // blue
+vec3 bottomRight(0,1,0); // green
+vec3 bottomLeft(1,1,0);  // yellow
+
+vector<vec3> leftSide( SCREEN_HEIGHT );
+vector<vec3> rightSide( SCREEN_HEIGHT );
+
+vector<vec3> colours(SCREEN_HEIGHT);
 
 /* ----------------------------------------------------------------------------*/
 /* FUNCTIONS                                                                   */
@@ -32,6 +41,9 @@ int main( int argc, char* argv[] )
 	screen = InitializeSDL( SCREEN_WIDTH, SCREEN_HEIGHT );
 	t = SDL_GetTicks();	// Set start value for timer.
 
+	Interpolate_v( topLeft, bottomLeft, leftSide );
+	Interpolate_v( topRight, bottomRight, rightSide );
+
 	while( NoQuitMessageSDL() )
 	{
 		Update();
@@ -42,6 +54,7 @@ int main( int argc, char* argv[] )
 	return 0;
 }
 
+
 void Update()
 {
 	// Compute frame time:
@@ -51,10 +64,27 @@ void Update()
 	cout << "Render time: " << dt << " ms." << endl;
 }
 
+// HOW TO RUN
+//	for( float step = 0; step < result_size; step++ ) {
+//			result[step] = Interpolate_f(start, end, step, result_size);
+//	}
 float Interpolate_f(float start, float end, float step, float max) {
 	return (end - start) * (step / (max - 1)) + start;
 }
 
+
+// HOW TO RUN
+// vector<vec3> result( 4 );
+// 	vec3 a(1,4,9.2);
+// 	vec3 b(4,1,9.8);
+// 	Interpolate_v( a, b, result );
+// 	for( int i=0; i<result.size(); ++i )
+// 	{
+// 		cout << "( "
+// 		<< result[i].x << ", "
+// 		<< result[i].y << ", "
+// 		<< result[i].z << " ) ";
+// 	}
 void Interpolate_v(vec3 a, vec3 b, vector<vec3> &result) {
 	float max_size = result.size();
 	for(float i = 0; i < max_size; i++) {
@@ -64,6 +94,11 @@ void Interpolate_v(vec3 a, vec3 b, vector<vec3> &result) {
 	}
 }
 
+// HOW TO RUN:
+// vector<float> result( 12 ); // Create a vector width 10 floats
+// Interpolate( 5, 14, result ); // Fill it with interpolated values
+// for( double i = 0; i < result.size(); i++ )
+// 	cout << result[i] << " "; // Print the result to the terminal
 void Interpolate( float start, float end, vector<float>& result ) {
 	float result_size = (float) result.size();
 
@@ -76,37 +111,24 @@ void Interpolate( float start, float end, vector<float>& result ) {
 	}
 }
 
+
 void Draw()
 {
 	if( SDL_MUSTLOCK(screen) ) {
 		SDL_LockSurface(screen);
 	}
 
-	// vector<float> result( 12 ); // Create a vector width 10 floats
-	// Interpolate( 5, 14, result ); // Fill it with interpolated values
-	// for( double i = 0; i < result.size(); i++ )
-	// 	cout << result[i] << " "; // Print the result to the terminal
-
-	vector<vec3> result( 4 );
-	vec3 a(1,4,9.2);
-	vec3 b(4,1,9.8);
-	Interpolate_v( a, b, result );
-	for( int i=0; i<result.size(); ++i )
-	{
-		cout << "( "
-		<< result[i].x << ", "
-		<< result[i].y << ", "
-		<< result[i].z << " ) ";
-	}
-
+	// TODO: future, add #includes and parralise loops like e.g.
+	// #pragma omp parallel for with Open MP 
 	for( int y=0; y<SCREEN_HEIGHT; ++y )
 	{
+		Interpolate_v(leftSide[y], rightSide[y], colours);
 
 		for( int x=0; x<SCREEN_WIDTH; ++x )
 		{
+			Interpolate_v(leftSide[x], rightSide[x], colours);
 
-			vec3 color( colour, 0.0, 0.0 );
-			PutPixelSDL( screen, x, y, color );
+			PutPixelSDL( screen, x, y, colours[y] );
 		}
 	}
 
