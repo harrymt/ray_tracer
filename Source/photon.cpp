a9e79452e83988f53c74e1b617ec7e98edb5aaf8
J_mie6 <jw14896@bristol.ac.uk>
2017-04-30 15:01:08 +0100

    a little better... still not sure what's causing the problem on that face...

--- Source/photon.cpp
+++ Source/photon.cpp
@@ -2,7 +2,7 @@
 
 #define NUM_PHOTONS 100000
 #define N 20
-#define SEARCH_RADIUS 0.2
+#define SEARCH_RADIUS 0.3
 
 extern glm::vec3 lightPos;
 extern Triangle* triangles;
@@ -29,7 +29,7 @@ std::vector<photon_t> generateMap()
 		dir = glm::normalize(dir);
 		glm::vec3 pos = lightPos;
 		glm::vec3 colour = glm::vec3(1, 1, 1);
-		float lum = 3.f;// / NUM_PHOTONS;
+		float lum = 5.f;// / NUM_PHOTONS;
 		Intersection closest;
 		bool first = true;
 		while (lum > 0)
@@ -55,7 +55,7 @@ std::vector<photon_t> generateMap()
 				colour.g *= triangle.color.g;
 				colour.b *= triangle.color.b;
 				photon.colour = colour;
-				photon.lum = 1.0;
+				photon.lum = 1.0f;
 
 				if (!first) photons.push_back(photon);
 				first = false;
@@ -114,7 +114,8 @@ glm::vec3 gather(vec3 pos, vec3 normal)
 		{
 			// additional constraint, we will ensure we only act on the plane normal to our intersection
 			float dot = glm::dot(pos - vec3(photon.x, photon.y, photon.z), normal);
-			if (0.0f <= dot && dot <= std::numeric_limits<float>::min())
+			if (-std::numeric_limits<float>::min()*10.0f <= dot && dot <= std::numeric_limits<float>::min()*10.0f)
+			//if (dot == 0.0f)
 			{
 				nearest.push_back(&photon);
 			}
@@ -126,10 +127,10 @@ glm::vec3 gather(vec3 pos, vec3 normal)
 	for (photon_t* photon : nearest)
 	{
 		float dist = glm::distance(vec3(photon->x, photon->y, photon->z), pos);
-		gather_colour += photon->colour *1.0f;// / sqrt(dist);
+		gather_colour += glm::clamp(photon->colour, vec3(0.f, 0.f, 0.f), vec3(1.f, 1.f, 1.f));// *1.0f / sqrt(dist);
 		sum_dist += 1.0f;// / sqrt(dist);
 	}
-	gather_colour /= 2.4f*sum_dist;
+	gather_colour /= 2.f*sum_dist;
 	
-	return glm::clamp(gather_colour, vec3(0.f, 0.f, 0.f), vec3(1.f, 1.f, 1.f));;
+	return glm::clamp(gather_colour, vec3(0.f, 0.f, 0.f), vec3(1.f, 1.f, 1.f));
 }
